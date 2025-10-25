c1e2f44b4fbdd8f16983b1bc898377b4afe3c345
PabloCarrai <pablohernanjuan@hotmail.com>
2025-10-25 20:42:05 -0300

    Va queriendo

--- factura_modelo.py
+++ factura_modelo.py
@@ -1,9 +1,31 @@
 import sqlite3
 
+
 class gestorDb:
-    def __init__(self):
-        self.conexion=sqlite3.connect("factura.db")
-        self.cursor=self.conexion.cursor()
+    def abrir_conexion(self):
+        try:
+            self.conexion = sqlite3.connect("facturas_digitales.db")
+            return self.conexion
+        except:
+            print("Error al conectar a la db")
 
-    
+    def registrar_Usuarios(self, datos):
+        try:
+            conexion = self.abrir_conexion()
+            mycursor = conexion.cursor()
+            sql = "insert into Clientes(nombre,apellido,email,telefono,direccion) values(?,?,?,?,?)"
+            mycursor.execute(sql, datos)
+            conexion.commit()
+        finally:
+            conexion.close()
 
+    def listar_Usuarios(self):
+        try:
+            conexion = self.abrir_conexion()
+            mycursor = conexion.cursor()
+            sql = "select cliente_id,nombre,apellido from Clientes"
+            mycursor.execute(sql)
+            resultados = mycursor.fetchall()
+            return resultados
+        finally:
+            conexion.close()
--- factura_vista.py
+++ factura_vista.py
@@ -1,5 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+from factura_modelo import gestorDb
+from tkinter import messagebox as ms
+
+
+conexion = gestorDb()
 
 
 class Ventana_Principal:
@@ -11,7 +16,7 @@ class Ventana_Principal:
         self.botonCrearUsuariolabelframeABMUsuario = Button(
             self.labelframeABMUsuario,
             text="Alta Usuario",
-            command=self.abrirVentanaSecundariaABMUsuario,
+            command=self.abrirVentanaSecundariaABMUsuarioAltaUsuario,
         )
         self.botonCrearUsuariolabelframeABMUsuario.grid(
             column=0, row=0, padx=10, pady=10
@@ -32,24 +37,177 @@ class Ventana_Principal:
         )
 
         self.botonListarUsuariolabelframeABMUsuario = Button(
-            self.labelframeABMUsuario, text="Alta Usuario"
+            self.labelframeABMUsuario,
+            text="Listar Usuario",
+            command=self.abrirVentanaSecundariaABMUsuarioListarUsuario,
         )
         self.botonListarUsuariolabelframeABMUsuario.grid(
             column=1, row=2, padx=10, pady=10
         )
         self.ventana.mainloop()
 
-    def abrirVentanaSecundariaABMUsuario(self):
-        Ventana_Alta_Usuario()
+    def abrirVentanaSecundariaABMUsuarioAltaUsuario(self):
+        ventana = Ventana_Alta_Usuario()
+
+    def abrirVentanaSecundariaABMUsuarioListarUsuario(self):
+        ventana = Ventana_Listar_Usuario()
 
 
 class Ventana_Alta_Usuario:
     def __init__(self):
         self.ventanaSecundaria = Toplevel()
-        self.etiquetaVentanaSecundaria = Label(
-            self.ventanaSecundaria, text="Probando Ando"
+        self.labelframeVentanaSecundariaABMUsuario = LabelFrame(
+            self.ventanaSecundaria, text="Datos del Usuario"
         )
-        self.etiquetaVentanaSecundaria.grid(column=0, row=0, padx=10, pady=10)
+        self.labelframeVentanaSecundariaABMUsuario.grid(
+            column=0, row=0, padx=10, pady=10
+        )
+        self.labelNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Label(
+            self.labelframeVentanaSecundariaABMUsuario, text="Nombre: "
+        )
+        self.labelNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=0, row=0, padx=10, pady=10
+        )
+        self.datoEntradaNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario = (
+            StringVar()
+        )
+        self.entradaNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Entry(
+            self.labelframeVentanaSecundariaABMUsuario,
+            textvariable=self.datoEntradaNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario,
+        )
+        self.entradaNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=1, row=0, padx=10, pady=10
+        )
+        self.labelApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Label(
+            self.labelframeVentanaSecundariaABMUsuario, text="Apellido: "
+        )
+        self.labelApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=0, row=1, padx=10, pady=10
+        )
+        self.datoEntradaApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario = (
+            StringVar()
+        )
+        self.entradaApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Entry(
+            self.labelframeVentanaSecundariaABMUsuario,
+            textvariable=self.datoEntradaApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario,
+        )
+        self.entradaApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=1, row=1, padx=10, pady=10
+        )
+        self.labelEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Label(
+            self.labelframeVentanaSecundariaABMUsuario, text="Correo: "
+        )
+        self.labelEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=0, row=2, padx=10, pady=10
+        )
+        self.datoEntradaEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario = (
+            StringVar()
+        )
+        self.entradaEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Entry(
+            self.labelframeVentanaSecundariaABMUsuario,
+            textvariable=self.datoEntradaEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario,
+        )
+        self.entradaEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=1, row=2, padx=10, pady=10
+        )
+        self.labelTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Label(
+            self.labelframeVentanaSecundariaABMUsuario, text="Telefono: "
+        )
+        self.labelTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=0, row=3, padx=10, pady=10
+        )
+        self.datoEntradaTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario = (
+            StringVar()
+        )
+        self.entradaTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Entry(
+            self.labelframeVentanaSecundariaABMUsuario,
+            textvariable=self.datoEntradaTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario,
+        )
+        self.entradaTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=1, row=3, padx=10, pady=10
+        )
+        self.labelDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Label(
+            self.labelframeVentanaSecundariaABMUsuario, text="Direccion: "
+        )
+        self.labelDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=0, row=4, padx=10, pady=10
+        )
+        self.datoEntradaDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario = (
+            StringVar()
+        )
+        self.entradaDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Entry(
+            self.labelframeVentanaSecundariaABMUsuario,
+            textvariable=self.datoEntradaDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario,
+        )
+        self.entradaDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=1, row=4, padx=10, pady=10
+        )
+        self.botonRegistrarABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Button(
+            self.labelframeVentanaSecundariaABMUsuario,
+            text="Registrar",
+            command=self.registrarUsuario,
+        )
+        self.botonRegistrarABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=0, row=5, padx=10, pady=10
+        )
+        self.botonCancelarABMUsuarioLabelFrameVentanaSecundariaABMUsuario = Button(
+            self.labelframeVentanaSecundariaABMUsuario,
+            text="Cancelar",
+            command=self.cancelarRegistro,
+        )
+        self.botonCancelarABMUsuarioLabelFrameVentanaSecundariaABMUsuario.grid(
+            column=1, row=5, padx=10, pady=10
+        )
+
+    def cancelarRegistro(self):
+        self.ventanaSecundaria.destroy()
+
+    def registrarUsuario(self):
+        datos = (
+            self.datoEntradaNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario.get(),
+            self.datoEntradaApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.get(),
+            self.datoEntradaEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario.get(),
+            self.datoEntradaTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.get(),
+            self.datoEntradaDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario.get(),
+        )
+        conexion.registrar_Usuarios(datos)
+        self.entradaNombreABMUsuarioLabelFrameVentanaSecundariaABMUsuario.delete(0, END)
+        self.entradaApellidoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.delete(
+            0, END
+        )
+        self.entradaEmailABMUsuarioLabelFrameVentanaSecundariaABMUsuario.delete(0, END)
+        self.entradaTelefonoABMUsuarioLabelFrameVentanaSecundariaABMUsuario.delete(
+            0, END
+        )
+        self.entradaDireccionABMUsuarioLabelFrameVentanaSecundariaABMUsuario.delete(
+            0, END
+        )
+        ms.showinfo("Vamos", "Registrado")
+
+
+class Ventana_Listar_Usuario:
+    def __init__(self):
+        self.ventana = Toplevel()
+        
+        self.labelFrameListarUsuarios = LabelFrame(self.ventana, text="Listar Usuarios")
+        self.labelFrameListarUsuarios.grid(column=0, row=0, padx=10, pady=10)
+        self.botonListarUsuarios = Button(
+            self.labelFrameListarUsuarios,
+            text="Listar Usuarios",
+            command=self.listarUsuarios,
+        )
+        self.botonListarUsuarios.grid(column=0, row=0, padx=10, pady=10)
+        self.listboxListarUsuarios = Listbox(self.labelFrameListarUsuarios)
+        self.listboxListarUsuarios.grid(column=0, row=1, padx=10, pady=10)
+
+    def listarUsuarios(self):
+        self.listboxListarUsuarios.delete(0, END)
+        resultado = conexion.listar_Usuarios()
+        for id, nombre, apellido in resultado:
+            self.listboxListarUsuarios.insert(
+                END, f"{id} {nombre} {apellido}"
+            )
+        self.listboxListarUsuarios.config(width=0, height=0)
 
 
 prueba = Ventana_Principal()
